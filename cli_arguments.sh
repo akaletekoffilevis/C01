0529bdc235d79a73364671ae403e9777d6e02683
akaletekoffilevis <l.akalete20@gmail.com>
2025-05-22 14:53:01 +0100

    com2

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
 
+#initialiser le compteur a 1
+read -p "Entrer vos arguments : " arg
+set $arg
+compt=1
+# Afficher chaque argument ligne par ligne
+while [ $# -gt 0 ]; do
+    echo "Argument $compt: $1"
+    #shift donne a $(n) la valeure de $(n+1) vers la gauche
+    shift
+    # copmt = copmt +1
+    ((compt++))
+done
+# n est un chiffre
 #mettre fin au script
 exit 0
