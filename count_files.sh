c48d23312e842ae60c344cde56d2474326a43249
akaletekoffilevis <koffilevis21@gmail.com>
2025-05-25 20:34:31 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -3,12 +3,14 @@
 #lire le nom du dossier
 read -p "Entrer le nom du dossier : " dossier 
 #verification de l'existence du dossier
-if [ -d "$dossier" ]; then
+#if [ -d "$dossier" ]; then
       #comptage du nombre de fichier
-      file_count=$(ls -1 "$dossier" | wc -l)
-      #afficher le nombre de fichiers present
-      echo "Le dossier $dossier contient $file_count fichier(s)."
-fi
-
+      #file_count=$(ls -1 "$dossier" #| wc -l)
+      #afficher le nombre de #fichiers present
+      #echo "Le dossier $dossier €contient $file_count fichier(s)."
+#fi
+count=(ls "$dossier" | wc -l)
+counts=$(echo $count)
+echo "Le dossier $dossier €contient $file_count fichier(s)."
 #mettre fin au script
 exit 0
